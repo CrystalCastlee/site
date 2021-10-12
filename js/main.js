8abb6ad5e7ebfb50a477829ae8cf47524fe6fea5
CrystalCastlee <86250704+CrystalCastlee@users.noreply.github.com>
2021-10-12 16:38:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const swiper1 = document.querySelector('.slider-container'),
+    swiper2 = document.querySelector('.swiper-container')
+    burger = document.querySelector('.burger'),
+    close = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu');
+
+let swiperSlider1 = new Swiper(swiper1, {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 105,
+});
+let swiperSlider2 = new Swiper(swiper2, {
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
+});
+
+burger.addEventListener('click', () => {
+    menu.classList.add('menu--visible');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('menu--visible');
+});
